ec825f575780681fc6dda14c580e394e596b69c9
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2016-03-05 20:52:42 -0500

    First attempt at backups ansible module

--- ansible/dimensiondata/dimensiondata_backup.py
+++ ansible/dimensiondata/dimensiondata_backup.py
@@ -4,7 +4,7 @@ DOCUMENTATION = '''
 module: dimensiondata_backup
 short_description: enable or disable backups for a host
 description:
-    - Creates, enables or disables backups for a host in the Dimension Data Cloud
+    - Creates, enables/disables backups for a host in the Dimension Data Cloud
 version_added: "1.9"
 options:
   state:
@@ -20,85 +20,31 @@ options:
     required: false
     default: null
     aliases: ['server_id']
-  name:
+  region:
     description:
-      - The name of the server you want to work on
-    required: false
-    default: null
-    aliases: []
-  image:
+      - The target region.
+    choices: ['na', 'eu', 'au', 'af', 'ap', 'latam', 'canada', 'canberra', 'id', 'in', 'il', 'sa']
+    default: na
+  service_plan:
     description:
-      - The image name to provision with
-    required: false
-    default: null
-    aliases: []
-  image_id:
+      - The service plan for backups.
+    choices: [Essentials, Advanced, Enterprise]
+    default: Essentials
+  verify_ssl_cert:
     description:
-      - The image_id to provisiong with
+      - Check that SSL certificate is valid.
     required: false
-    default: null
-    aliases: []
-  vlan:
+    default: true
+  wait:
     description:
-      - The name of the vlan to provision to
+      - Should we wait for the task to complete before moving onto the next
     required: false
-    default: null
-    aliases: []
-  vlan_id:
+    default: false
+  wait_time:
     description:
-      - The vlan_id to provision to
+      - Only applicable if wait is true.  This is the amount of time in seconds to wait
     required: false
-    default: null
-    aliases: []
-  network:
-    description:
-      - The name of the network to provision to
-    required: false
-    default: null
-    aliases: []
-  network_id:
-    description:
-      - The network_id to provision to
-    required: false
-    default: null
-    aliases: []
-  network_domain:
-    description:
-      - The name of the network domain to provision to
-    required: false
-    default: null
-    aliases: []
-  network_domain_id:
-    description:
-      - The network_domain_id to provision to
-    required: false
-    default: null
-    aliases: []
-  admin_password:
-    description:
-      - The administrator account password for a new server
-    required: false
-    default: null
-    aliases: []
-  description:
-    description:
-      - The description for the new node
-    required: false
-    default: null
-    aliases: []
-  memory_gb:
-    description:
-      - The amount of memory for the new host to have in Gb
-    required: false
-    default: null
-    aliases: []
-  unique_names:
-    description:
-      - By default Dimension Data allows the same name for multiple servers this will make sure we don't create a new server if the name already exists
-    required: false
-    default: 'no'
-    aliases: []
-    choices: ['yes', 'no']
+    default: 120
 
 author:
     - "Jeff Dunham (@jadunham1)"
@@ -109,147 +55,144 @@ EXAMPLES = '''
 # export DIDATA_USER=<username>
 # export DIDATA_PASSWORD=<password>
 
-# Basic create node example
+# Basic enable backups example
 
-- didata:
-    vlan_id: '{{ vlan_id }}'
-    network_domain_id: '{{ network_domain_id }}'
-    image: 'RedHat 7 64-bit 2 CPU'
-    name: ansible-test-image
-    admin_password: fakepass
+- dimensiondata_backup:
+    server_ids:
+      - '7ee719e9-7ae9-480b-9f16-c6b5de03463c'
+
+# Basic remove backups example
+- dimensiondata_backup:
+    server_ids:
+      - '7ee719e9-7ae9-480b-9f16-c6b5de03463c'
+    state: absent
+
+# Full options enable
+- dimensiondata_backup:
+    server_ids:
+      - '7ee719e9-7ae9-480b-9f16-c6b5de03463c'
+    state: present
+    wait: yes
+    wait_time: 500
+    service_plan: Advanced
+    verify_Sssl_cert: no
 '''
-import os
-import json
 
-HAS_LIBCLOUD = True
+RETURN = '''
+servers:
+    description: list of servers this worked on
+    returned: Always
+    type: list
+    contains: server_ids processed
+'''
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.dimensiondatacloud import *
 try:
-    from libcloud.compute.drivers.dimensiondata import DimensionDataNodeDriver
-    from libcloud.common.dimensiondata import DEFAULT_REGION
-except ImportError:
+    from libcloud.common.dimensiondata import DimensionDataAPIException
+    from libcloud.backup.drivers.dimensiondata import DimensionDataBackupDriver
+    import libcloud.security
+    HAS_LIBCLOUD = True
+except:
     HAS_LIBCLOUD = False
 
-def module_key_die_if_none(module, key):
-    v = module.params[key]
-    if v is None:
-        module.fail_json(msg='Unable to load %s' % key)
-    return v
+POLLING_INTERVAL = 2
 
 
-def get_image_id(client, module, location):
-    if module.params['image_id'] is not None:
-        return module.params['image_id']
-    if module.params['image'] is None:
-        module.fail_json(msg='Need to specify either an image_id or image to create a node')
-
-    image_match_name = module.params['image']
-    images = client.list_images(location)
-    images.extend( client.ex_list_customer_images(location) )
-
-    matched_images = list(filter(lambda x: x.name == image_match_name, images))
-
-    if len(matched_images) < 1:
-        module.fail_json(msg='No images matched this name')
-    elif len(matched_images) > 1:
-        module.fail_json(msg='Multile images matched this please specify one of the image ids')
-
-    return matched_images[0].id
-
-
-def create_node(client, module):
+def handle_backups(module, client):
     changed = False
-    name = module_key_die_if_none(module, 'name')
-    if module.params['unique_names']:
-        node_list = client.list_nodes(ex_name=name)
-        if len(node_list) >= 1:
-            return (changed, [node.id for node in node_list])
-
-    admin_password = module_key_die_if_none(module, 'admin_password')
-    vlan_id = module_key_die_if_none(module, 'vlan_id')
-    network_id = module.params['network_id']
-    network_domain_id = module.params['network_domain_id']
-    if not network_domain_id and not network_id:
-        moduule.fail_json(msg='Need either a network_id (MCP1.0) or network_domain_id (MCP_2.0) to create a server')
-
-    dd_vlan = client.ex_get_vlan(vlan_id)
-    image_id = get_image_id(client, module, dd_vlan.location.id)
-    node = client.create_node(name, image_id, admin_password,
-                       module.params['description'],
-                       ex_network=network_id,
-                       ex_network_domain=network_domain_id,
-                       ex_vlan=vlan_id,
-                       ex_memory_gb=module.params['memory_gb'])
-    return (True, node.id)
-
-
-
-def stoporstart_servers(client, module, desired_state):
-    changed = False
-
-    servers = module_key_die_if_none(module, 'server_ids')
-    node_list = []
-    for server in servers:
-        node = client.ex_get_node_by_id(server)
-        node_list.append({
-            'id': node.id,
-            'prev_state': node.state,
-            'desired_state': desired_state
-        })
-        if node.state == 'terminated':
-            node.state = 'stopped'
-        if desired_state != node.state:
-            if desired_state == 'running':
-                client.ex_start_node(node)
-                changed = True
-            elif desired_state == 'stopped':
-                client.ex_shutdown_graceful(node)
-                changed = True
-
-    return (changed, node_list)
-
-def core(module):
-    try:
-        username = os.environ['DIDATA_USER']
-        password = os.environ['DIDATA_PASSWORD']
-    except KeyError, e:
-        module.fail_json(msg='unable to find key %s' % e.message)
-
-    if not username or not password:
-        module.fail_json(msg='here unable to find username %s and password %s')
-
-    try:
-        region = os.environ['REGION']
-    except KeyError:
-        region = DEFAULT_REGION
-
-    client = DimensionDataNodeDriver(username, password, region)
     state = module.params['state']
-    if state == 'present':
-        return enable_backup(client, module)
-    elif state == 'absent':
-        return remove_backup(client, module)
-    else:
-        module.fail_json(msg='Unhandled state transition')
+    service_plan = module.params['service_plan']
+
+    for server_id in module.params['server_ids']:
+        try:
+            backup_details = client.ex_get_backup_details_for_target(server_id)
+        except DimensionDataAPIException as e:
+            # Libcloud client returns throws an error if no backups for client
+            # we'll catch this and set backup details to None instead of dying
+            if e.msg.endswith('has not been provisioned for backup'):
+                backup_details = None
+            else:
+                module.fail_json("Problem finding backup info for host: %s"
+                                 % e.msg)
+
+        if backup_details is None and state == 'absent':
+            continue
+        elif backup_details is not None and state == 'absent':
+            changed = True
+            disable_backup_for_server(client, module, server_id)
+        elif backup_details is None and state == 'present':
+            changed = True
+            enable_backup_for_server(client, module, server_id, service_plan)
+        elif backup_details is not None and state == 'present':
+            if backup_details.service_plan != service_plan:
+                changed = True
+                modify_backup_for_server(client, module,
+                                         server_id, service_plan)
+        else:
+            module.fail_json(msg="Unhandle state")
+
+    module.exit_json(changed=changed, msg='Enabled host',
+                     servers=module.params['server_ids'])
+
+
+def enable_backup_for_server(client, module, server_id, service_plan):
+    extra = {'servicePlan': service_plan}
+    client.create_target(None, server_id, extra=extra)
+    if module.params['wait'] is True:
+        try:
+            client.connection.wait_for_state(
+                'NORMA', client.ex_get_backup_details_for_target,
+                POLLING_INTERVAL, module.params['wait_time'], server_id
+            )
+        except DimensionDataAPIException as e:
+            module.fail_json(msg='Backup did not enable in time: %s' % e.msg)
+
+
+def disable_backup_for_server(client, module, server_id):
+    client.delete_target(server_id)
+
+
+def modify_backup_for_server(client, module, server_id, service_plan):
+    extra = {'servicePlan': service_plan}
+    client.update_target(server_id, extra=extra)
 
 
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            state = dict(default='present', choices=['present', 'absent']),
-            server_ids = dict(type='list', aliases=['server_id']),
-            plan = dict(choices=['ADVANCED', 'ESSENTIALS', 'ENTERPRISE'])
+        argument_spec=dict(
+            region=dict(default='na', choices=['na', 'eu', 'au', 'af', 'ap',
+                                               'latam', 'canada', 'canberra',
+                                               'id', 'in', 'il', 'sa']),
+            state=dict(default='present', choices=['present', 'absent']),
+            server_ids=dict(required=True, type='list',
+                            aliases=['server_id']),
+            service_plan=dict(default='Essentials',
+                              choices=['Advanced',
+                                       'Essentials',
+                                       'Enterprise']),
+            verify_ssl_cert=dict(required=False, default=True, type='bool'),
+            wait=dict(required=False, default=False, type='bool'),
+            wait_time=dict(required=False, default=120, type='int')
         )
     )
     if not HAS_LIBCLOUD:
-        module.fail_json(msg='libcloud >= 1.0.0pre required for this module')
+        module.fail_json(msg='libcloud is required for this module.')
 
-    try:
-        (changed, data) = core(module)
-    except (Exception), e:
-        module.fail_json(msg=str(e))
+    # set short vars for readability
+    credentials = get_credentials()
+    if credentials is False:
+        module.fail_json("User credentials not found")
+    user_id = credentials['user_id']
+    key = credentials['key']
+    region = 'dd-%s' % module.params['region']
+    verify_ssl_cert = module.params['verify_ssl_cert']
 
-    module.exit_json(changed=changed, instances=data)
+    # Instantiate driver
+    libcloud.security.VERIFY_SSL_CERT = verify_ssl_cert
+    client = DimensionDataBackupDriver(user_id, key, region=region)
 
-from ansible.module_utils.basic import *
+    handle_backups(module, client)
 
 if __name__ == '__main__':
         main()
